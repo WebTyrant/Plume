08d11d1e1f24945cd7e3f526df88b952c52ad7aa
Neil Johnston <neil@neiljohnston.net>
2018-06-26 12:53:30 -0700

    Event on addfeature

--- firemap.js
+++ firemap.js
@@ -1,12 +1,12 @@
 var layerMapper = [{
     name: 'DriveBC',
     type: 'geojson',
-    endpoint: 'https://drivebctokml.now.sh/geojson',
+    endpoint: 'https://drivebctokml.now.sh/geojson'
   },
   {
     name: 'ActiveFirePoints',
     type: 'geojson',
-    endpoint: 'https://services6.arcgis.com/ubm4tcTYICKBpist/arcgis/rest/services/BCWS_ActiveFires_PublicView/FeatureServer/0/query?where=FIRE_STATUS+%3C%3E+%27Not+Active%27&objectIds=&time=&geometry=&geometryType=esriGeometryEnvelope&inSR=&spatialRel=esriSpatialRelEnvelopeIntersects&resultType=none&distance=0.0&units=esriSRUnit_Meter&returnGeodetic=false&outFields=*&returnGeometry=true&multipatchOption=xyFootprint&maxAllowableOffset=&geometryPrecision=&outSR=&datumTransformation=&applyVCSProjection=false&returnIdsOnly=false&returnCountOnly=false&returnExtentOnly=false&returnDistinctValues=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&having=&resultOffset=&resultRecordCount=&returnZ=false&returnM=false&returnExceededLimitFeatures=true&quantizationParameters=&sqlFormat=none&f=pgeojson&token=',
+    endpoint: 'https://services6.arcgis.com/ubm4tcTYICKBpist/arcgis/rest/services/BCWS_ActiveFires_PublicView/FeatureServer/0/query?where=FIRE_STATUS+%3C%3E+%27Not+Active%27&objectIds=&time=&geometry=&geometryType=esriGeometryEnvelope&inSR=&spatialRel=esriSpatialRelEnvelopeIntersects&resultType=none&distance=0.0&units=esriSRUnit_Meter&returnGeodetic=false&outFields=*&returnGeometry=true&multipatchOption=xyFootprint&maxAllowableOffset=&geometryPrecision=&outSR=&datumTransformation=&applyVCSProjection=false&returnIdsOnly=false&returnCountOnly=false&returnExtentOnly=false&returnDistinctValues=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&having=&resultOffset=&resultRecordCount=&returnZ=false&returnM=false&returnExceededLimitFeatures=true&quantizationParameters=&sqlFormat=none&f=pgeojson&token='
   }
 ];
 
@@ -34,7 +34,6 @@ function styleFunction(feature, resolution) {
   var geom_name = feature.getGeometry().getType();
   // return styles[geom_name]
   // so styles['Point'] or styles['LineString']
-  console.log(feature.getId());
   return styles[geom_name];
 };
 
@@ -66,10 +65,15 @@ layerMapper.forEach((loadLayer) => {
     style: styleFunction,
   });
 
+  loadLayer.source.on( 'addfeature', function (event) {
+    const feature = event.feature;
+//    feature.layerName = layerName;
+    //feature.set('layer', 'test');
+    console.log(feature);
+  });
+  
   map.addLayer(loadLayer.layer);
 });
-
-console.log(map.getLayers());
 
 
 map.on('click', (evt) => {
